a03504885b84335c67e253966220fefc337ba330
endzrijana <anastep1919@gmail.com>
2026-08-31 16:26:54 +0200

    Fix deployed frontend connection

--- backend/main.py
+++ backend/main.py
@@ -387,7 +387,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://diplomski-b5lh.onrender.com",
+        "https://diplomski-psi.vercel.app",
 
     ],
 
